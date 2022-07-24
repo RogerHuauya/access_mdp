79f534109b35b333683d7d6bb8c6bb241c52ce57
roger <rhuauyam@uni.pe>
2022-07-24 15:41:39 -0500

    python support added

---
+++ python/communication.py
@@ -0,0 +1,23 @@
+import requests
+import datetime as dt
+import pandas as pd
+from os.path import exists
+from os import remove
+URL_AUTH_USERS = 'https://srf-webapp-prd.azurewebsites.net/api/colaborador/hash'
+URL_ATTENDANCE_USERS = 'https://srf-webapp-prd.azurewebsites.net/api/asistencia'
+
+user_register_file = 'user_register.csv'
+
+response = requests.get(URL_AUTH_USERS)
+auth_users = response.json()
+df_auth = pd.json_normalize(auth_users)
+df_auth.to_csv('auth_users.csv')
+print(df_auth)
+
+if exists(user_register_file):
+    df = pd.read_csv(user_register_file)
+    remove(user_register_file)
+    for index, row in df.iterrows():
+        data = {'Mac': '00:04:4B:E5:AD:A1', 'Colaboradores':[{"FechaHora": str(dt.datetime.now()), 'Hash': row['hash'], 'Dni': row['dni']}]}
+        response_post = requests.post(url = URL_ATTENDANCE_USERS, data = data)
+
