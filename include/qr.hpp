53371b51f94dc88a1890b0f9f925f72590c7a384
Roger <rhuauyam@uni.pe>
2022-08-14 11:57:08 -0500

    bug fixed

--- include/qr.hpp
+++ include/qr.hpp
@@ -1,6 +1,6 @@
 #include <string>
 #include <vector>
 extern std::string password_key;
-std::vector<string> hash_arr, dni_arr, name_arr;
+extern std::vector<string> hash_arr, dni_arr, name_arr;
 bool qrDetector(float active_time);
 void readAuthFile();
